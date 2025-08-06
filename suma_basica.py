872558d7bca8698b1358cf9fcd16f4c21e22f08c
TomasCostantini <tomascostantini899@gmail.com>
2025-08-06 10:21:49 -0300

    Create suma_basica.py

---
+++ suma_basica.py
@@ -0,0 +1,12 @@
+#Algoritmo sensillo pasa sumar 3 numeros ingresaods por consola
+
+#Solicita al usuario ingresar tres números
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+numero3 = int(input("Ingrese el tercer número: "))
+
+#Suma los tres números
+suma = numero1 + numero2 + numero3
+
+#Muestra el resultado
+print("La suma de los tres números es:", suma)
